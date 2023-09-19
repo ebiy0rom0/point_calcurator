aa92c8420d87af6df77b2b51e4f9e7c05a9daf7a
Kenya Kojima <kk.n0h2a2l3.end@gmail.com>
2023-09-19 23:16:12 +0900

    [add]prividing chakra UI

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,10 +1,9 @@
-import Header from "../components/Header.tsx";
+import { ChakraProvider } from "chakra-ui";
 
 export default function App({ children }: { children: React.ReactNode }) {
   return (
-    <>
-      <Header />
+    <ChakraProvider>
       {children}
-    </>
+    </ChakraProvider>
   );
 }
